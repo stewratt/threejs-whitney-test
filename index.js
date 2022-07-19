bc497d53fac18657bf0a7e188776982b6a7e70ad
stewratt <74335366+stewratt@users.noreply.github.com>
2022-07-19 14:47:56 -0400

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const scene = new THREE.Scene()
 
 const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({
-    color:0xff5733
+    color:0xffce30
 })
 
 const mesh = new THREE.Mesh(geometry,material)
